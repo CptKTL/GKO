37014dbcd41fcdc3dc07ad4154e8f864814710ba
Kimmo Lappalainen <kimmolapp@gmail.com>
2016-07-31 14:21:12 +0300

    jotain lisätty?

---
+++ Valmiit/vt3/vt3/t3/JänisDemo/HorizontalBar/horizontalBar.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HorizontalBar
+{
+    public partial class horizontalBar : UserControl
+    {
+        Color alku;
+        Color loppu;
+        Color alkuDefault = Color.Black;
+        Color loppuDefault = Color.Red;
+        int paksuus;
+        Point sijainti;
+
+        Graphics canvas;
+
+        private Brush pensseli1;
+        private Brush pensseli2;
+
+        [Category("Ominaisuudet"),
+            Description("Gradientin alkuväri"),
+            Browsable(true)]
+        public Color Alku
+        {
+            get
+            {
+                return alku;
+            }
+            set
+            {
+                alku = value;
+                Gradient_SizeChanged(this, null);
+            }
+        }
+
+        [Category("Ominaisuudet"),
+            Description("Gradientin loppuväri"),
+            Browsable(true)]
+        public Color Loppu
+        {
+            get
+            {
+                return loppu;
+            }
+            set
+            {
+                loppu = value;
+                Gradient_SizeChanged(this, null);
+            }
+        }
+
+        [Category("Ominaisuudet"),
+            Description("Gradientin paksuus"),
+            DefaultValue(0),
+            Browsable(true)]
+        public int Paksuus
+        {
+            get { return paksuus; }
+            set
+            {
+                paksuus = value;
+                Height = value;
+                Gradient_SizeChanged(this, null);
+            }
+        }
+
+        [Category("Ominaisuudet"),
+            Description("Gradientin sijainti"),
+            Browsable(true)]
+        public Point Sijainti
+        {
+            get { return Location; }
+            set { Location = value; Gradient_SizeChanged(this, null); }
+        }
+
+
+        public void ResetAlku()
+        {
+            Alku = alkuDefault;
+        }
+
+        public void ResetLoppu()
+        {
+            Loppu = loppuDefault;
+        }
+
+        public bool ShouldSerializeAlku()
+        {
+            return alku != alkuDefault;
+        }
+
+        public bool ShouldSerializeLoppu()
+        {
+            return loppu != loppuDefault;
+        }
+
+        public horizontalBar()
+        {
+            alku = alkuDefault;
+            loppu = loppuDefault;
+            paksuus = 30;
+            BackColor = Color.FromArgb(0, 0, 0, 0);
+            InitializeComponent();
+
+            pensseli1 = new System.Drawing.Drawing2D.LinearGradientBrush(new PointF(0, 0), new PointF(0, paksuus / 2.0f), alku, loppu);
+            pensseli2 = new System.Drawing.Drawing2D.LinearGradientBrush(new PointF(0, paksuus / 2), new PointF(sijainti.X, sijainti.Y + paksuus), loppu, alku);
+        }
+
+        /// <summary>
+        /// Muutetaan vastaamaan ikkunan leveyttä.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Gradient_SizeChanged(object sender, EventArgs e)
+        {
+            pensseli1.Dispose();
+            pensseli2.Dispose();
+            pensseli1 = new System.Drawing.Drawing2D.LinearGradientBrush(new PointF(sijainti.X, sijainti.Y), new PointF(0, sijainti.Y + paksuus / 2), alku, loppu);
+            pensseli2 = new System.Drawing.Drawing2D.LinearGradientBrush(new PointF(sijainti.X, sijainti.Y + paksuus / 2), new PointF(sijainti.X, sijainti.Y + paksuus), loppu, alku);
+
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Piirrellään uudelleen. Joku off-by-one virhe?
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            canvas = e.Graphics;
+            canvas.FillRectangle(pensseli1, 0, sijainti.Y + 1, this.ClientSize.Width, (paksuus / 2.0f));
+            canvas.FillRectangle(pensseli2, 0, sijainti.Y + 1 + paksuus / 2.0f, this.ClientSize.Width, paksuus / 2.0f);
+
+            base.OnPaint(e);
+        }
+
+        /// <summary>
+        /// Muutetaan leveyttä vastaamaan ikkunan leveyttä.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void horizontalBar_ParentChanged(object sender, EventArgs e)
+        {
+            if (Parent != null)
+            {
+                Parent.Resize += new EventHandler(Parent_Resize);
+                if (AutoSize)
+                {
+                    this.Width = Parent.ClientSize.Width;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Muutetaan leveyttä vastaamaan ikkunan leveyttä.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Parent_Resize(object sender, EventArgs e)
+        {
+            if (AutoSize)
+            {
+                this.Width = Parent.ClientSize.Width;
+            }
+        }
+
+    }
+}
---
+++ Valmiit/vt3/vt3/t3/JänisDemo/Scroller/scroller.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scroller
+{
+    public partial class scroller : UserControl
+    {
+        private int nopeus = 3;
+        Graphics canvas;
+        int lev = 0;
+        int kor = 0;
+
+        List<Point> koords;
+        int max = 200;
+        int koord = 0;
+        Point sijainti;
+
+        public Point Sijainti
+        {
+            get
+            {
+                return sijainti;
+            }
+            set
+            {
+                sijainti = value;
+                PaivitaKoordinaatit();
+            }
+        }
+
+        public string Teksti
+        {
+            get
+            {
+                return Text;
+            }
+            set
+            {
+                Text = value;
+                PaivitaKoordinaatit();
+            }
+        }
+        public int Nopeus
+        {
+            get
+            {
+                return nopeus;
+            }
+            set
+            {
+                nopeus = value;
+                max = 10000 / nopeus;
+                PaivitaKoordinaatit();
+            }
+        }
+
+        public scroller()
+        {
+            InitializeComponent();
+            koords = new List<Point>();
+            Teksti = "";
+            PaivitaKoordinaatit();
+            ForeColor = Color.Blue;
+        }
+
+        /// <summary>
+        /// Piirrellään.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            canvas = e.Graphics;
+            canvas.DrawString(this.Text, SystemFonts.DefaultFont, Brushes.White, new Point(0, 0));
+
+        //    base.OnPaint(e);
+        }
+
+        /// <summary>
+        /// Liikutellaan tekstiä.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            koord++;
+            if (koord >= max) koord = 0;
+
+            Location = koords[koord];
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Päivitetään koordinaatit vastaamaan ikkunan kokoa
+        /// </summary>
+        private void PaivitaKoordinaatit()
+        {
+            Size = TextRenderer.MeasureText(Text, Font);
+            koords.Clear();
+            for (int i = 0; i < max; i++)
+            {
+                int kor = 150;
+                
+                int x = (int)((lev + TextRenderer.MeasureText(Text, Font).Width) - (1.0 * i / max) * (lev + 2 * TextRenderer.MeasureText(Text, Font).Width));
+                int y = (int)(Math.Sin(2.0 * Math.PI * lev / 300 * i / max) * (kor / 6) + Sijainti.Y);
+                koords.Add(new Point(x, y));
+            }
+        }
+
+        /// <summary>
+        /// Jos koko muuttuu niin piirretään uudelleen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void scroller_SizeChanged(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        /// <summary>
+        /// Jos vanhempi muuttuu niin lisätään kuuntelija Parent.Rezizelle.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void scroller_ParentChanged(object sender, EventArgs e)
+        {
+            if (Parent != null)
+            {
+                Parent.Resize += new EventHandler(Parent_Resize);
+
+                this.lev = Parent.ClientSize.Width;
+                this.kor = Parent.ClientSize.Height;
+                PaivitaKoordinaatit();
+            }
+        }
+
+        /// <summary>
+        /// Päivitetään ikkunan kokotiedot oikeiksi.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Parent_Resize(object sender, EventArgs e)
+        {
+            this.kor = Parent.ClientSize.Height;
+            this.lev = Parent.ClientSize.Width;
+            PaivitaKoordinaatit();
+        }
+    }
+}
