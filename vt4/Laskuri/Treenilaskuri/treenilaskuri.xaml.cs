0f85ce5927945fa90477b56cd837d73501d27dd8
Kimmo Lappalainen <kimmolapp@gmail.com>
2016-08-03 23:15:23 +0300

    vt4 taso 2,5

--- vt4/Laskuri/Treenilaskuri/treenilaskuri.xaml.cs
+++ vt4/Laskuri/Treenilaskuri/treenilaskuri.xaml.cs
@@ -18,9 +18,9 @@ namespace Treenilaskuri
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class treenilaskuri : UserControl
     {
-        public UserControl1()
+        public treenilaskuri()
         {
             InitializeComponent();
         }
