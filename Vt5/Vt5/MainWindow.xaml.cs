ef67d2c730c037ac7cd9cba474237eeb81e06ac4
Kimmo Lappalainen <kimmolapp@gmail.com>
2016-08-05 23:22:00 +0300

    vt5 osittain tehty

--- Vt5/Vt5/MainWindow.xaml.cs
+++ Vt5/Vt5/MainWindow.xaml.cs
@@ -15,14 +15,73 @@ using System.Windows.Shapes;
 
 namespace Vt5
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class Henkilo
+    {
+        private string etunimi;
+        private string sukunimi;
+        private int ika = 11;
+        private string kansallisuus;
+        private static Random r = new Random(2016);
+        public Henkilo()
+        {
+            string[] etunimet = { "John", "Jane", "Jack", "Jill", "Jules", "Jake", "Julian", "Jonathan", "Julia", "Judith" };
+            string[] sukunimet = { "Doe", "Smith", "Jones", "Taylor", "Brown", "Wilson", "Davis", "Johnson", "Robinson" };
+            string[] kansat = { "USA", "Kanada", "Australia" };
+            Ika = r.Next(1, 110);
+            Kansallisuus = kansat[r.Next(0, kansat.Length)];
+            Etunimi = etunimet[r.Next(0, etunimet.Length)];
+            Sukunimi = sukunimet[r.Next(0, sukunimet.Length)];
+        }
+
+        public string Kansallisuus
+        {
+            get { return kansallisuus; }
+            set { kansallisuus = value; }
+        }
+
+        public string Etunimi
+        {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
+
+        public string Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
+
+        public int Ika
+        {
+            get { return ika; }
+            set { if (value > 0 && value <= 110) ika = value; }
+        }
+
+
+
+    }
+
     public partial class MainWindow : Window
     {
+        private List<Henkilo> henkilot;
+
+        public List<Henkilo> Henkilot
+        {
+            get { return henkilot; }
+            set { henkilot = value; }
+        }
+
         public MainWindow()
         {
+            henkilot = new List<Henkilo>();
+            for (int i = 0; i < 32; i++)
+            {
+                henkilot.Add(new Henkilo());
+            }
+
             InitializeComponent();
+
+
         }
     }
 }
