e633b9ba7ba15ad263bb94ccdfe782b8d2a12b2b
Kimmo Lappalainen <kimmolapp@gmail.com>
2016-08-05 19:53:49 +0300

    nyt vt4 kokonaan?

---
+++ vt4/t3/Laskuri/Laskuri/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laskuri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// New komento suoritetaan.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            UusiTreeniLaskuri();
+        }
+
+
+        /// <summary>
+        /// New komento voidaan suorittaa aina.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+
+        /// <summary>
+        /// Luo uuden treenilaskurin ikkunaan.
+        /// </summary>
+        private void UusiTreeniLaskuri()
+        {
+            Treenilaskuri.treenilaskuri laskuri = new Treenilaskuri.treenilaskuri();
+
+            TreeniLaskurit.Children.Add(laskuri);
+        }
+    }
+}
---
+++ vt4/t3/Laskuri/TimeTextBox/timeTextBox.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeTextBox
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class timeTextBox : UserControl
+    {
+        /// <summary>
+        /// TimeTextBoxin Aika.
+        /// </summary>
+        public TimeSpan Time
+        {
+            get { return (TimeSpan)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+
+        // Using a DependencyProperty as the backing store for Time.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof(TimeSpan), typeof(timeTextBox), new FrameworkPropertyMetadata(TimeSpan.Zero, FrameworkPropertyMetadataOptions.AffectsRender, onTimeChanged, changeTime));
+
+
+        /// <summary>
+        /// Muutoksen oikeellisuustarkistus.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static object changeTime(DependencyObject obj, object value)
+        {
+            TimeSpan time = (TimeSpan)value;
+
+            if (time.CompareTo(new TimeSpan(24, 0, 0)) >= 0)
+            {
+                throw new Exception("Suurempi kuin 24h.");
+            }
+            else if (time.CompareTo(new TimeSpan(0, 0, 0)) < 0)
+            {
+                throw new Exception("Pienenpi kuin 0.");
+            }
+
+            return time;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="args"></param>
+        public static void onTimeChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            /// Tyhjä.
+        }
+
+
+        public timeTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
